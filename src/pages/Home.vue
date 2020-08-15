5a7f944d46d22704839dd4469939daf69b1f9ac0
danielmarques28 <danielmarques7@hotmail.com>
2020-08-15 10:34:48 -0300

    Change home page design, add search input and delete button

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,23 +1,56 @@
 <template>
   <div class="home">
     <div class="center-div">
+      <label for="search" class="label">
+        Upload de arquivo
+      </label>
       <input
         type="file"
         name="file"
         id="file"
+        class="input-file"
         @change="upload"
-      >
-      <!-- <button v-on:click="this.upload">
-        Upload
-      </button> -->
+      />
+
+      <div class="search-div">
+        <label for="search" class="label">
+          Pesquisa
+        </label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Nº do ID ou nome do arquivo"
+          class="input"
+          v-model="search"
+        />
+      </div>
+
+      <div v-if="files.length === 0" class="empty-msg">
+        <h1>Não há arquivos salvos.</h1>
+      </div>
+
       <div
-        v-for="(file, index) in files"
+        v-for="(file, index) in filteredFiles"
         v-bind:key="index"
       >
-        arquivo: {{ file.name }}
-        <button v-on:click="download(file.id, file.name)">
-          Download
-        </button>
+        <div class="file">
+          {{ file.id }} - {{ file.name }}
+          <div class="buttons">
+            <button
+              v-on:click="download(file.id, file.name)"
+              class="btn"
+            >
+              Download
+            </button>
+            <button
+              v-on:click="deleteFile(file.id)"
+              class="btn btn-red"
+            >
+              Excluir
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -31,11 +64,30 @@ export default {
   data() {
     return {
       files: [],
-      uploadFile: {}
+      search: ''
     }
   },
   mounted() {
     this.getAllFiles();
+  },
+  computed: {
+    filteredFiles() {
+      if (!this.search) {
+        return this.files;
+      } else {
+        const id = Number(this.search);
+        if(!isNaN(id)) {
+          return this.files.filter((file) => {
+            return file.id === id;
+          });
+        } else {
+          let re = new RegExp(this.search);
+          return this.files.filter((file) => {
+            return re.test(file.name.toLowerCase());
+          });
+        }
+      }
+    }
   },
   methods: {
     getAllFiles() {
@@ -49,6 +101,7 @@ export default {
       if (!files.length)
         return;
       let formData = new FormData();
+      formData.append('file', files[0]);
       fileAPI.uploadFile(formData)
         .then(() => {
           this.getAllFiles();
@@ -64,6 +117,12 @@ export default {
           document.body.appendChild(fileLink);
           fileLink.click();
         });
+    },
+    deleteFile(fileId) {
+      fileAPI.deleteFile(fileId)
+        .then(() => {
+          this.getAllFiles();
+        });
     }
   }
 }
@@ -73,17 +132,83 @@ export default {
 .home {
   display: flex;
   justify-content: center;
-  align-items: center;
   width: 100vw;
   height: 100vh;
-  background: grey;
+  background: #f6f8fa;
+  overflow-y: auto;
   .center-div {
-    width: 65vw;
-    min-height: 85vh;
-    overflow-y: auto;
-    border-radius: 5px;
-    padding: 0.5rem;
-    background: white;
+    width: 22rem;
+    margin: 2rem 0;
+    @media only screen and (max-width: 600px) {
+      width: 17.5rem;
+    }
+    .label {
+      margin-left: 0.25rem;
+    }
+    .input-file {
+      width: 100%;
+      margin-top: 0.2rem;
+      margin-bottom: 1rem;
+      border: none;
+      border-radius: 5px;
+      background: #2c68b1;
+      color: white;
+      font-weight: bold;
+      font-size: 1rem;
+      box-shadow: 0 2px 2px rgba(0, 0, 0, 0.25);
+    }
+    .search-div {
+      .input {
+        width: 20.9rem;
+        height: 2.5rem;
+        margin-top: 0.2rem;
+        margin-bottom: 1rem;
+        border-style: solid;
+        border-width: 1px;
+        border-radius: 5px;
+        padding: 0 0.5rem;
+        @media only screen and (max-width: 600px) {
+          width: 16.5rem;
+        }
+      }
+    }
+    .empty-msg {
+      text-align: center;
+      h1 {
+        font-weight: normal;
+      }
+    }
+    .file {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0.6rem;
+      margin-bottom: 0.6rem;
+      box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+      border-radius: 5px;
+      background: white;
+      overflow: hidden;
+      .buttons {
+        .btn {
+          height: 2.5rem;
+          margin: 0 0.1rem;
+          border: none;
+          border-radius: 5px;
+          background: #2c68b1;
+          color: white;
+          font-weight: bold;
+          font-size: 14px;
+          cursor: pointer;
+          &-red {
+            background: white;
+            border-style: solid;
+            border-width: 1px;
+            color: #cb2431;
+            border-color: rgba(27,31,35,.15);
+          }
+        }
+      }
+    }
   }
 }
 </style>
